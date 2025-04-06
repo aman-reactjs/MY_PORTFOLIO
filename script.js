641f2290256ec7f5e28b7186e7eb7f1b68957b79
Aman Sharma <amanvashishtha606@gmail.com>
2025-04-06 20:16:28 +0530

    Update UI

---
+++ script.js
@@ -0,0 +1,9 @@
+// width: 100px;
+// height: auto;
+// /* background-color: red; */
+// padding: 0 0 0 20px;
+// margin: 0 0 0 150px;
+// text-decoration: none;
+// font-size: 15px;
+// text-decoration: none;
+// list-style: none;
